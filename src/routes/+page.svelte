19fc14f1911597be783cd28d81110b957b060e38
nightguarder <nigthguarder@gmail.com>
2023-03-08 14:36:48 +0100

    uninstall typescript

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
-    export let data;
 </script>
-<h1>Main page</h1>
-<p>Welcome back {data.user.name}!</p>
+<div class="md:container display:block ">
+<h1 class="font-medium text-center">Welcome to MedicineChat system powered by GPT 3.5</h1>
+<div class="md:container pt-2">
+    <p class="text-lg text-center"> Please proceed by Login to the system.</p>
+    <p class="text-lg text-center">If you are new here: <a href="/register">Get Started</a></p> 
+</div>
+  </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   darkMode: 'class',
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
