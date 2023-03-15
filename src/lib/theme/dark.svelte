0821bff15a0e84aa2a5888f07406c60aa1e82bd5
nightguarder <nigthguarder@gmail.com>
2023-03-15 20:25:11 +0100

    dark mode fix

--- src/lib/theme/dark.svelte
+++ src/lib/theme/dark.svelte
@@ -1,38 +1,34 @@
 <script>
-    import { browser, dev, version } from '$app/environment';
+	import { browser, dev, version } from '$app/environment';
     //global variable
-    let boolTheme = true;
+	let boolTheme = true;
     function toggleTheme() {
-        const htmlElement = document.querySelector('html');
-        let currentTheme = localStorage.getItem('theme')||'light';
-        currentTheme = currentTheme === 'light' ? 'dark' : 'light';
-        htmlElement.setAttribute('data-theme', currentTheme);
-        localStorage.setItem('theme', currentTheme);
-    }
-    if(browser){
-        if (localStorage.theme === 'dark' ||(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-			boolTheme = true;
-            localStorage.setItem('theme','dark');
-		} 
-        else {
-			boolTheme = false;
-            localStorage.setItem('theme','light');
-		}
+		console.log("checked " + boolTheme);
+		if (boolTheme) {
+    		document.documentElement.setAttribute(THEME_ATTRIBUTE, 'dark')
+    		localStorage.setItem('theme', 'light')
+  		} 
+  		else {
+   		document.documentElement.setAttribute(THEME_ATTRIBUTE, 'light')
+    	localStorage.setItem('theme', 'light')
+  		}
+	console.log(THEME_ATTRIBUTE);
+  	boolTheme = !boolTheme
     }
 </script>
-	<input  checked={boolTheme} on:click={toggleTheme} type="checkbox" id="theme-toggle" />
+	<input checked={boolTheme} on:click={toggleTheme} type="checkbox" id="theme-toggle" />
 	<label class="relative m-1 " for="theme-toggle"></label>
 <style lang="postcss">
 	#theme-toggle {
-        @apply invisible
+        @apply invisible;
 	}
 	#theme-toggle + label {
 		@apply  cursor-pointer h-10 w-10 p-5  rounded-full duration-300 content-[''];
 	}
-	#theme-toggle:not(:checked) + label {
+	#theme-toggle:checked + label {
 		@apply bg-amber-400;
 	}
-	#theme-toggle:checked + label {
+	#theme-toggle:not(:checked) + label {
 		@apply bg-transparent;
 		box-shadow: inset -18px -16px 1px 1px #ffffff;
 	}
