acd8ebf413093bc7b19e86115d971a3a06f322e2
nightguarder <nigthguarder@gmail.com>
2023-03-19 00:43:44 +0100

    main page updated

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -46,7 +46,7 @@
 <slot></slot>
 
 <!-- Footer -->
-<footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
+<footer class="footer footer-center p-10 bg-base-200 font-medium text-base rounded">
   <div class="grid grid-flow-col gap-4">
     <a href="./login" class="link link-hover">Login</a> 
     <a href="./register" class="link link-hover">Register</a>
