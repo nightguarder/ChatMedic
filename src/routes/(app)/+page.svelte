d73f657ca0ad97fd8f869fa862abc723fb2e9a3a
nightguarder <nigthguarder@gmail.com>
2023-04-25 22:50:49 +0200

    responsive design missing

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -16,17 +16,19 @@
 		>
 			Ai Assistant powered by <em>GPT 3.5</em>
 		</h2>
-		<div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg">
+		<div class="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg whitespace-pre-line">
 			<p>
-				An easy-to-use AI assistant that can help medical experts make informed decisions and
+				An easy-to-use AI assistant that can help medical experts 
+				make informed decisions and
 				provide the best possible care for their patients.
 			</p>
 		</div>
 		<br />
-		<div class="py-2">
+		<!-- TODO: Responsive design! -->
+		<div class="py-2 w-full lg:text-center">
 			<a
 				href="#one"
-				class="btn btn-primary normal-case text-center w-full px-8 py-4 mt-2 text-white rounded-md md:mb-0 hover:bg-cyan-600 md:w-auto"
+				class="btn btn-primary normal-case text-center px-8 py-4 mt-2 text-white rounded-md md:mb-0 hover:bg-cyan-600 lg:max-w-xl md:w-auto"
 				data-rounded="rounded-md">Start Here</a
 			>
 		</div>
