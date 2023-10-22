3c372226cdee468476aeeb48df40931708257cf5
cyrils <nigthguarder@gmail.com>
2023-10-22 21:23:48 +0200

    Install OpenAI

---
+++ playwright.config.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    webServer: {
+        command: 'npm run build && npm run preview',
+        port: 4173
+    },
+    testDir: 'tests'
+};
+exports.default = config;
